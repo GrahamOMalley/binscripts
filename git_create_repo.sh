edae5d628f93773a21ab27a1c219b924d94060ad
Gom <gomgomgom@gmail.com>
2012-10-29 18:11:10 +0000

    adding binscripts with git_create_script

---
+++ git_create_repo.sh
@@ -0,0 +1,9 @@
+#! /bin/bash
+
+USER='GrahamOMalley'
+PASS='s1teswap'
+REPO=$1
+
+curl -u "$USER:$PASS" https://api.github.com/user/repos -d '{"name":"'$REPO'"}'
+git remote add origin git@github.com:$USER/$REPO.git
+git push origin master
